f825aee1e884b91aeaa949ac4bc382649ee69a5e
Allemander <matthewaminyard@gmail.com>
2022-03-01 20:04:27 -0500

    matt

---
+++ script.js
@@ -0,0 +1,6 @@
+const keys = {
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    number: "0123456789",
+    symbol: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
+  }
